0f4234470f8de390c25a38bb6d7a742a124c8135
LAH4285 <ahyun4285@gmail.com>
2023-12-16 21:22:12 +0900

    [UPDATE] User 기능 구현
    User 기능 구현 및 에러코드와 Security작업

---
+++ Board/demo/src/main/java/com/example/demo/home/homeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public String loginform(){return "login";}
+    @GetMapping("/join")
+    public String joinform(){return "join";}
+
+}
